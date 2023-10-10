4a7c589246d38672640cb0d52821227b8366cba0
Rohaniscoder <109896047+Rohaniscoder@users.noreply.github.com>
2023-10-10 16:32:34 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -657,8 +657,6 @@ async def send_all(bot, userid, files, ident):
                    [
                      InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=CHNL_LNK),
                      InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                   ],[
-                     InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}') #Don't change anything without contacting me @LazyDeveloperr
                    ]
                    ]
                 )
